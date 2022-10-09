583191ad54be01c5a6fec37a2dc2ef9967a80302
rodom_macbook <enjoying1018@naver.com>
2022-10-10 02:15:38 +0900

    requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&qkx19nsa7b5_r#^tr)1w+&+ka-z3d9ro1m8w$+%)eheb=72+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
